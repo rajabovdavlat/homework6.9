d8899fd4a61cc98e6343fca2cc262b167131950c
rajabovdavlat <rajabovoff1@gmail.com>
2025-06-15 14:29:22 +0500

    rrfr

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,29 +1,32 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-function useFetchData() {
+const BASE_URL = "http://localhost:3000"; 
+
+const params = "/condidate";
+
+function useAxios() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch("/data.json");
-        if (!response.ok) throw new Error("Ошибка при загрузке");
-        const json = await response.json();
-        setData(json);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(BASE_URL + params);
+      setData(response.data)
+    } catch (error) {
+       setError(error)
+    } finally {
+      setLoading(false)
+    }
+  };
 
+  useEffect(() => {
     getData();
-  }, []);
+  }, [params]);
 
   return { data, error, loading };
-}
+};
 
-export default useFetchData;
+export default useAxios;
